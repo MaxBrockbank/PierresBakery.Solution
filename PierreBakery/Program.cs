49b45e831c4e9eb4fbcb0354fb9ba8c455ce71de
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-11 11:29:42 -0800

    add CLI logic and Payment method for validation payment method

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace PierreBakery.Models
 {
@@ -7,7 +8,34 @@ namespace PierreBakery.Models
   {
     static void Main()
     {
+      Console.WriteLine("Welcome to Pierre's Bakery. Today we have bread and pastires for sale!");
+      Console.WriteLine("What's your name valued customer?");
+      string name = Console.ReadLine();
+      Console.WriteLine($"How many loaves of bread would you like {name}?");
+      int bread = int.Parse(Console.ReadLine());
+      Console.WriteLine($"Sounds good! {bread} loaves of bread. How many pastries can I get for you?");
+      int pastry = int.Parse(Console.ReadLine());
 
+      Bread breadOrder = new Bread(bread);
+      Pastry pastryOrder = new Pastry(pastry);
+
+      Console.WriteLine($"So for {bread} loaves of bread and {pastry} pastries your total comes out to ${breadOrder.OrderCost() + pastryOrder.OrderCost()}. Cash or Card today?");
+      string paymentMethod = Console.ReadLine();
+      Console.WriteLine(Payment(paymentMethod, name));
+    }
+    
+    static string Payment(string method, string name)
+    {
+      Regex validPayment = new Regex(@"card|cash" , RegexOptions.IgnoreCase);
+      
+      if(validPayment.IsMatch(method))
+      {
+        return $"Have a good day {name}!";
+      }
+      else 
+      {
+        return "This isn't a valid payment method. If you want your goods you gotta pay bub.";
+      }
     }
   }
 }
