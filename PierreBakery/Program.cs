99f4e33f20e094f48a5354acb4e428fc9973bf7d
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-11 15:42:00 -0800

    adjust the CLI user interaction text

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -14,16 +14,17 @@ namespace PierreBakery.Models
 
       Console.WriteLine("We have two kind of bread today: French for $5 and Sourdough for $10. Which kind would you like?");
       string bread = Console.ReadLine();
+      bread = char.ToUpper(bread[0]) + bread.Substring(1);
       Console.WriteLine($"And how many loaves of {bread} bread would you like?");
       int breadAmount = int.Parse(Console.ReadLine());
       Bread breadOrder = new Bread();
       int breadCost = breadOrder.TypeOfBread(bread, breadAmount);
 
-      Console.WriteLine($"Sounds good! {bread} loaves of bread. How many pastries can I get for you? Today they are $2 each or 3 for $5.");
+      Console.WriteLine($"Sounds good! {breadAmount} loaves of {bread} bread. How many pastries can I get for you? Today they are $2 each or 3 for $5."); 
       int pastry = int.Parse(Console.ReadLine());
       Pastry pastryOrder = new Pastry(pastry);
 
-      Console.WriteLine($"So for {bread} loaves of bread and {pastry} pastries your total comes out to ${breadCost + pastryOrder.OrderCost()}. Cash or Card today?");
+      Console.WriteLine($"So for {breadAmount} loaves of {bread}bread and {pastry} pastries your total comes out to ${breadCost + pastryOrder.OrderCost()}. Cash or Card today?");
       string paymentMethod = Console.ReadLine();
       Console.WriteLine(Payment(paymentMethod, name));
     }
