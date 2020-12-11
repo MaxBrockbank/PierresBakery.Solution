658b442ec3f8314a182162e254609788da5c0a16
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-11 08:30:06 -0800

    add project file structure

---
+++ PierreBakery.Tests/ModelsTests/PierreBakeryTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PierreBakery.Models;
+
+namespace PierreBakery.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+
+  }
+
+  public class PastryTests
+  {
+    [TestMethod]
+    
+  }
+}
